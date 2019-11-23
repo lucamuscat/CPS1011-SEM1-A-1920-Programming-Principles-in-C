1b9d16a2d1fe48c10bd530f152ce1647155bd132
Luca Muscat <luca.muscat.19@um.edu.mt>
2019-11-23 12:08:19 +0100

    Implemented str_shuffle()

--- task1/lib/functions_str.c
+++ task1/lib/functions_str.c
@@ -28,6 +28,18 @@ void str_generate(mystr_t* array, int i){
 }
 
 void str_shuffle(char array[10][STR_N]){
+/*
+  Same philosophy as shuffle() in functions.c but uses strcpy to swap
+  strings around.
+*/
+void str_shuffle(mystr_t* array){
 	char tmp[STR_N];
 	int j;
+	for (size_t i = 0; i < array->size; ++i) {
+		j = random(i);
+		strcpy(tmp, array -> nums[j]);
+		strcpy(array -> nums[j], array -> nums[i]);
+		strcpy(array -> nums[i], tmp);
+	}
+}
 }
