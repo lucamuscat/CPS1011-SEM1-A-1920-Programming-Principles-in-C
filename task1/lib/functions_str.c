8b1e910e0483732806b3b44d5262fc67123f12b4
Luca Muscat <luca.muscat.19@um.edu.mt>
2019-11-23 12:10:05 +0100

    Implemented str_sort()

--- task1/lib/functions_str.c
+++ task1/lib/functions_str.c
@@ -64,4 +64,48 @@ char* str_target(mystr_t *array){
 		return "error";
 	return string_ptr;
 }
+void int_swap(int* a, int* b){
+	//Swap ints
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+void str_swap(char* a, char* b){
+	char temp[STR_N];
+	strcpy(temp, a);
+	strcpy(a, b);
+	strcpy(b, temp);
+}
+int* str_to_int(mystr_t *array){
+	int *temp = malloc(sizeof(int) * array->size);
+	for(size_t i = 0; i < array -> size; ++i){
+		temp[i] = -1;
+		for(size_t j = 0; j < 11; ++j){
+			if(strcmp(array->nums[i], NUMS_STR_REPR[j]) == 0){
+				temp[i] = j;
+				break;
+			}
+		}
+	}
+	return temp;
+}
+
+mystr_t str_sort(mystr_t *array){
+	mystr_t temp = *array;
+	size_t min_index;
+	int *int_repr = str_to_int(array);
+	for(size_t i = 0; i < array->size; ++i){
+		min_index = i;
+		for(size_t j = i +1; j < array->size; ++j){
+			if(int_repr[j] < int_repr[min_index])
+				min_index = j;
+		}
+		int_swap(&int_repr[i], &int_repr[min_index]);
+		/*
+		   temp.nums is a 2d array so the address operator won't need
+		   to be used
+		*/
+		str_swap(temp.nums[i], temp.nums[min_index]);
+	}
+	return temp;
 }
