3acebacac240d4eb17d3408d90b1ddf0e55b7087
Luca Muscat <luca.muscat.19@um.edu.mt>
2019-12-08 21:01:26 +0100

    Implemented receiveMessage

--- task2/lib/basic_queue.c
+++ task2/lib/basic_queue.c
@@ -186,3 +186,22 @@ int restoreQ(MsgQs_t *q, int identifier) {
 	}
 	free(message);
 }
+
+int receiveMessages(MsgQs_t *q, int identifier, size_t num_of_messages) {
+	node *node;
+	for (size_t i = 0; i < q->index; ++i) {
+		if(q->nodes[i]->ID == identifier){
+			node = q->nodes[i];
+		}
+	}
+	if(node == NULL)
+		return -1;
+
+	while(node->index>0 && num_of_messages>0){
+		printf("%d: %s", node->index, node->messages[node->index-1]);
+		free(node->messages[node->index]);
+		node->index--;
+		num_of_messages--;
+	}
+	return SUCCESS;
+}
